05b365d7cec5016f60f95c7f6adbf8e36683c7d6
Garefild <Garefild2@gmail.com>
2023-04-11 00:28:14 +0300

    create basic configuration for library

---
+++ projects/angular-redux2/store/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    moduleNameMapper: {
+        '^@angular-redux2/store$': '<rootDir>/projects/angular-redux2/store/src/public-api.ts',
+        '^@angular-redux2/store/testing$': '<rootDir>/projects/angular-redux2/store/testing/public-api.ts',
+    },
+    modulePathIgnorePatterns: [
+        '<rootDir>/dist',
+        '<rootDir>/package.json'
+    ],
+    globals: {
+        'ts-jest': {
+            diagnostics: false
+        }
+    }
+};
