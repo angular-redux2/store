31a18aa8eca00b59e60361de514112b9d5c37476
Garefild <Garefild2@gmail.com>
2023-04-14 02:08:00 +0300

    fractal.interface: add SubstoreFlag, SELECTION_KEY, SUBSTORE_KEY

--- projects/angular-redux2/store/src/interfaces/fractal.interface.ts
+++ projects/angular-redux2/store/src/interfaces/fractal.interface.ts
@@ -1,4 +1,10 @@
 /**
+ * Angular-redux types
+ */
+
+import type { NgRedux } from '../services/ng-redux.service';
+
+/**
  * Key used to store the action type in the Redux store's state tree.
  * This is used to track dispatched actions and perform time-travel debugging.
  * It is recommended that you do not modify or access this value directly.
@@ -14,4 +20,35 @@ export const ACTION_KEY = '&_ACTION';
  */
 
 export const LOCAL_REDUCER_KEY = '&_SUBSTORE_LOCAL_REDUCER';
+
+/**
+ * Key to store the substore instance in the decorated class instance.
+ * This key is used by the `WithSubStore` decorator to cache the substore instance created for the class instance.
+ * @type {string}
+ */
+
+export const SUBSTORE_KEY = '&_SUBSTORE';
+
+/**
+ * Key to access the cached selection object of a decorated class instance.
+ * The cached selection object contains the properties and methods that are decorated with the `@Select()` decorator.
+ * The value of this constant is used as a property name in the instance object to store the selection object.
+ * The actual value of the constant is not important as long as it is unique and not likely to conflict with other property names.
+ * @type {string}
+ */
+
+export const SELECTION_KEY = '&_SELECTION';
+
+/**
+ * Represents a flag used to store information related to a substore.
+ *
+ * @typedef {Object} SubstoreFlag
+ * @property {NgRedux<any>} instance - The instance of the substore.
+ * @property {string} cachePath - The path of the substore that is cached.
+ */
+
+export type SubstoreFlag = {
+    instance: NgRedux<any>;
+    cachePath: string;
+}
 
