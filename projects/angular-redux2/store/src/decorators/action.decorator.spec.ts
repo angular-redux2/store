d8708250a4e5e60402a2ee08adc4ee6e10ce7e5e
Garefild <Garefild2@gmail.com>
2023-04-14 01:45:12 +0300

    action.decorator: add action decorator

---
+++ projects/angular-redux2/store/src/decorators/action.decorator.spec.ts
@@ -0,0 +1,45 @@
+/**
+ * Angular-redux
+ */
+
+import { Action } from "./action.decorator";
+
+describe('Action', () => {
+    class ExampleReducer {
+        static actions: any;
+
+        @Action
+        exampleFunction(payload: any) {
+            return payload;
+        }
+    }
+
+    test('should create an action creator for a specific function', () => {
+        expect(ExampleReducer.actions.exampleFunction('test payload')).toEqual({
+            type: 'ExampleReducer/exampleFunction',
+            payload: 'test payload',
+        });
+    });
+
+    test('TestReducer.addBug() should return an action object with the correct type and payload', () => {
+        // Instantiate the test subject
+        new ExampleReducer();
+
+        // Call the action creator without a payload
+        const result = (ExampleReducer as any).actions['exampleFunction']();
+
+        // Call the action creator with a payload
+        const resultPayload = (ExampleReducer as any).actions['exampleFunction']({ test: 'test' });
+
+        // Assert that the action creator without a payload returns an action object with the correct type and no payload
+        expect(result).toEqual({
+            type: 'ExampleReducer/exampleFunction'
+        });
+
+        // Assert that the action creator with a payload returns an action object with the correct type and payload
+        expect(resultPayload).toEqual({
+            type: 'ExampleReducer/exampleFunction',
+            payload: { test: 'test' }
+        });
+    });
+});
---
+++ projects/angular-redux2/store/src/decorators/action.decorator.ts
@@ -0,0 +1,55 @@
+/**
+ * Import third-party types
+ */
+
+import type { AnyAction } from 'redux';
+
+/**
+ * A decorator function that generates an action creator for a specific function.
+ *
+ * @example
+ * ```typescript
+ * export class RoutesReducer extends AbstractReducer {
+ *
+ *     // optional static var to allow to add type's for auto-complete
+ *     // ActionPayload< payload interface / state >
+ *     static override actions: ActionPayload<RoutesReducer>;
+ *
+ *      @Action
+ *      isLogin(state: Auth, action: AnyAction): Auth {
+ *          return { isLoggedIn: !state.isLoggedIn };
+ *      }
+ * }
+ *
+ * public isLogin() {
+ *     this.ngRedux.dispatch(
+ *         RoutesReducer.actions.isLogin()
+ *     );
+ * }
+ * ```
+ *
+ * @param {*} target - The target object on which to apply the decorator.
+ * @param {string} functionName - The name of the function to create the action for.
+ * @constructor
+ */
+
+export const Action = (target: any, functionName: string): void => {
+    const TargetClass = target.constructor;
+    const actionName = `${TargetClass.name }/${functionName}`;
+
+    if (!TargetClass.actions) {
+        TargetClass.actions = {};
+    }
+
+    TargetClass.actions[functionName] = (payload: any) => {
+        const action: AnyAction = {
+            type: actionName
+        };
+
+        if (payload) {
+            action['payload'] = payload;
+        }
+
+        return action;
+    };
+};
