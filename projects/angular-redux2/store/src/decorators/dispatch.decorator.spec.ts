f5d3372bece5fa3bf187a85be396d55bb7b980ca
Garefild <Garefild2@gmail.com>
2023-04-14 18:53:01 +0300

    dispatch.decorator: add dispatch decorator

---
+++ projects/angular-redux2/store/src/decorators/dispatch.decorator.spec.ts
@@ -0,0 +1,96 @@
+/**
+ * Angular-redux
+ */
+
+import { Dispatch } from "./dispatch.decorator";
+import { DecoratorFlagComponent } from '../components/decorator-flag.component';
+
+/**
+ * Initialize global test mocks
+ */
+
+jest.mock('../components/decorator-flag.component');
+
+class TestClass {
+    @Dispatch
+    addBug(numberOfLines: number): any {
+        return { type: 'ADD_LOC', payload: numberOfLines };
+    }
+
+    @Dispatch
+    emptyBug(numberOfLines: number): any {
+        return null;
+    }
+
+    @Dispatch
+    trueBug(numberOfLines: number): any {
+        return true;
+    }
+}
+
+describe('Dispatch', () => {
+    let targetClass: any;
+    let mockDispatch = jest.fn();
+
+    beforeEach(() => {
+        targetClass = new TestClass();
+        (DecoratorFlagComponent as jest.Mock).mockImplementation(() => ({
+            store: {
+                dispatch: mockDispatch
+            }
+        }));
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('should dispatch the result to the store when returning an object', () => {
+        const result = targetClass.addBug(5);
+
+
+        expect(result).toEqual({ type: 'ADD_LOC', payload: 5 });
+        expect(mockDispatch).toHaveBeenCalledWith({ type: 'ADD_LOC', payload: 5 });
+    });
+
+    test('should dispatch the result to the store when returning a falsy value', () => {
+        const result = targetClass.emptyBug(5);
+
+        expect(result).toBeNull();
+        expect(mockDispatch).toHaveBeenCalledWith(null);
+    });
+
+    test('should dispatch the result to the store when returning a truthy value that is not an object', () => {
+        const result = targetClass.trueBug(5);
+
+        expect(result).toBe(true);
+        expect(mockDispatch).toHaveBeenCalledWith(true);
+    });
+
+    test('should dispatch the result to the store when using an external function', () => {
+        const functionName = 'testFunction';
+        const externalFunction = jest.fn(() => ({ type: 'TEST', payload: { value: 'test' } }));
+
+        Dispatch(targetClass, functionName);
+        targetClass[functionName] = externalFunction;
+        const result = targetClass[functionName]();
+
+
+        expect(result).toEqual({ type: 'TEST', payload: { value: 'test' } });
+        expect(externalFunction).toHaveBeenCalled();
+        expect(mockDispatch).toHaveBeenCalledWith({ type: 'TEST', payload: { value: 'test' } });
+    });
+
+    test('should dispatch the result to the store when using a bound property', () => {
+        const functionName = 'testFunction';
+        const boundProperty = jest.fn(() => ({ type: 'TEST', payload: { value: 'test' } }));
+
+        Dispatch(targetClass, functionName);
+        targetClass[functionName] = boundProperty;
+        const result = targetClass[functionName]();
+
+        expect(result).toEqual({ type: 'TEST', payload: { value: 'test' } });
+        expect(boundProperty).toHaveBeenCalled();
+        expect(mockDispatch).toHaveBeenCalledWith({ type: 'TEST', payload: { value: 'test' } });
+    });
+});
---
+++ projects/angular-redux2/store/src/decorators/dispatch.decorator.ts
@@ -0,0 +1,86 @@
+/**
+ * Angular-redux
+ */
+
+import { DecoratorFlagComponent } from '../components/decorator-flag.component';
+
+/**
+ * Decorator that automatically dispatches the return value of a decorated function
+ * to the Redux store using `ngRedux.dispatch()`.
+ *
+ * ```typescript
+ * // These dispatches will be scoped to the substore as well, as if you
+ * // had called ngRedux.configureSubStore(...).dispatch(numLines).
+ *
+ * @Dispatch
+ * addCode(numLines: any) {
+ *     // Dispatching from the sub-store ensures this component instance's
+ *     // subStore only sees the 'ADD_LOC' action.
+ *     return { type: 'ADD_LOC', payload: numLines };
+ * }
+ * ```
+ *
+ * In the above example, the `addCode` function is decorated with `@Dispatch`.
+ * Anytime the `addCode` function is called, its return value will automatically
+ * be dispatched to the Redux store using `ngRedux.dispatch()`.
+ *
+ * @remarks
+ * This decorator assumes that the component instance has an `ngRedux` property
+ * that refers to a valid `NgRedux` instance. If the component instance does not
+ * have an `ngRedux` property, an error will be thrown.
+ *
+ * @param {any} target - The target class instance.
+ * @param {string | symbol | number} functionName - The name of the decorated function.
+ * @param {PropertyDescriptor} descriptor - The object PropertyDescriptor.
+ * @return The PropertyDescriptor.
+ */
+
+export function Dispatch(target: any, functionName: string | symbol | number, descriptor?: PropertyDescriptor): any {
+    let originalMethod: any;
+    descriptor = descriptor || Object.getOwnPropertyDescriptor(target, functionName);
+
+    const wrapped = function (this: any, ...args: any[]) {
+        const result = originalMethod.apply(this, args);
+
+        if (result !== false) {
+            const instanceManager = new DecoratorFlagComponent(this);
+            const store = instanceManager.store;
+
+            if (store) {
+                store.dispatch(result);
+            }
+        }
+
+        return result;
+    };
+
+    /**
+     * User for boundProperty | externalFunction
+     * that can be undefined
+     *
+     *     @Dispatch
+     *     externalFunction: (value: string) => any;
+     *
+     *     @Dispatch
+     *     boundProperty = (value: string): any => ({
+     *         type: 'TEST',
+     *         payload: { value },
+     *     });
+     */
+
+    if (descriptor === undefined) {
+        const dispatchDescriptor: PropertyDescriptor = {
+            get: () => wrapped,
+            set: setMethod => (originalMethod = setMethod),
+        };
+
+        Object.defineProperty(target, functionName, dispatchDescriptor);
+
+        return dispatchDescriptor;
+    }
+
+    originalMethod = descriptor.value;
+    descriptor.value = wrapped;
+
+    return descriptor;
+}
