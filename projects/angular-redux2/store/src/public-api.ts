b8e31b9696dca84e3a83ee92721d1743b7b9ad94
Garefild <Garefild2@gmail.com>
2023-04-15 02:25:16 +0300

    public-api: export some functionality

--- projects/angular-redux2/store/src/public-api.ts
+++ projects/angular-redux2/store/src/public-api.ts
@@ -14,7 +14,6 @@ export * from './services/sub-store.service';
 export * from './services/reducer.service';
 
 // Interfaces
-export * from './interfaces/sync.interface';
 export * from './interfaces/store.interface';
 
 // Decorators
@@ -22,6 +21,3 @@ export * from './decorators/action.decorator';
 export * from './decorators/select.decorator';
 export * from './decorators/dispatch.decorator';
 export * from './decorators/substore.decorator';
-
-// Components
-export { reduxSyncMiddleware } from './components/sync.component';
