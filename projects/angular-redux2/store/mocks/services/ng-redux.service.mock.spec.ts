4a80addca6aeb9f1ad9c12708932bfb8a0b721f5
Garefild <Garefild2@gmail.com>
2023-04-15 05:22:05 +0300

    add mocks for testing

---
+++ projects/angular-redux2/store/mocks/services/ng-redux.service.mock.spec.ts
@@ -0,0 +1,212 @@
+/**
+ * Import third-party library
+ */
+
+import { map } from 'rxjs';
+import { toArray } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+
+/**
+ * Import third-party types
+ */
+
+import type { Observable } from 'rxjs';
+
+/**
+ * Angular-redux
+ */
+
+import { Dispatch } from '../../src/decorators/dispatch.decorator';
+import { Select, Select$ } from '../../src/decorators/select.decorator';
+
+/**
+ * Angular-redux
+ */
+
+import { NgRedux } from '../../src/services/ng-redux.service';
+
+/**
+ * Mocks
+ */
+
+import { MockNgRedux } from './ng-redux.service.mock';
+import { MockNgReduxModule } from '../ng-redux.module.mock';
+
+@Component({
+    template: 'whatever',
+    selector: 'test-component'
+})
+class TestComponent {
+    @Select('foo') readonly obs$: Observable<number>;
+    @Select$('bar', (obs$: any) => obs$.pipe(map((x: any) => 2 * x)))
+
+    readonly barTimesTwo$: Observable<number>;
+    readonly baz$: Observable<number>;
+
+    constructor(public ngRedux: NgRedux<any>) {
+        this.baz$ = ngRedux.select('baz');
+    }
+
+    @Dispatch
+    test() {
+        return {
+            type: 'UPDATE'
+        };
+    }
+}
+
+describe('NgReduxMock angular component', () => {
+    /**
+     * Spy vars
+     */
+
+    let select: any;
+    let dispatch: any;
+
+    /**
+     * Before each test
+     */
+
+    beforeEach(() => {
+        MockNgRedux.reset();
+        select = jest.spyOn(MockNgRedux.store, 'select');
+        dispatch = jest.spyOn(MockNgRedux.store, 'dispatch');
+
+        TestBed.configureTestingModule({
+            declarations: [ TestComponent ],
+            imports: [ MockNgReduxModule ],
+        }).compileComponents();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+        jest.restoreAllMocks();
+    });
+
+    test('should call mock select', () => {
+        const testComponent = TestBed.createComponent(TestComponent).componentInstance;
+
+        testComponent.obs$.subscribe(jest.fn());
+        expect(select).toBeCalled();
+    });
+
+    test('should call mock dispatch', () => {
+        const testComponent = TestBed.createComponent(TestComponent).componentInstance;
+
+        testComponent.test();
+        expect(dispatch).toBeCalled();
+    });
+
+    test('should reset stubs used by @select', (done) => {
+        const instance = TestBed.createComponent(TestComponent).componentInstance;
+
+        const stub1 = MockNgRedux.getSelectorStub('foo');
+        stub1.next(1);
+        stub1.next(2);
+        stub1.complete();
+
+        instance.obs$.pipe(toArray()).subscribe((values: number[]) => {
+            expect(values).toEqual([ 1, 2 ]);
+            done();
+        });
+
+        MockNgRedux.reset();
+
+        // Reset should result in a new stub getting created.
+        const stub2 = MockNgRedux.getSelectorStub('foo');
+        expect(stub1 === stub2).toBe(false);
+
+        stub2.next(3);
+        stub2.complete();
+
+        instance.obs$.pipe(toArray()).subscribe((values: number[]) => {
+            expect(values).toEqual([ 3 ]);
+            done();
+        });
+    });
+
+    test('should reset stubs used by @select$', (done) => {
+        const instance = TestBed.createComponent(TestComponent).debugElement
+            .componentInstance;
+
+        const stub1 = MockNgRedux.getSelectorStub('bar');
+        stub1.next(1);
+        stub1.next(2);
+        stub1.complete();
+
+        instance.barTimesTwo$.pipe(toArray()).subscribe((values: number[]) => {
+            expect(values).toEqual([ 2, 4 ]);
+            done();
+        });
+
+        MockNgRedux.reset();
+
+        // Reset should result in a new stub getting created.
+        const stub2 = MockNgRedux.getSelectorStub('bar');
+        expect(stub1 === stub2).toBe(false);
+
+        stub2.next(3);
+        stub2.complete();
+
+        instance.obs$.pipe(toArray()).subscribe((values: number[]) => {
+            expect(values).toEqual([ 6 ]);
+            done();
+        });
+    });
+
+    test('should reset stubs used by .select', (done) => {
+        const instance = TestBed.createComponent(TestComponent).debugElement
+            .componentInstance;
+
+        const stub1 = MockNgRedux.getSelectorStub('baz');
+        stub1.next(1);
+        stub1.next(2);
+        stub1.complete();
+
+        instance.baz$.pipe(toArray()).subscribe((values: number[]) => {
+            expect(values).toEqual([ 1, 2 ]);
+            done();
+        });
+
+        MockNgRedux.reset();
+
+        // Reset should result in a new stub getting created.
+        const stub2 = MockNgRedux.getSelectorStub('baz');
+        expect(stub1 === stub2).toBe(false);
+
+        stub2.next(3);
+        stub2.complete();
+
+        instance.obs$.pipe(toArray()).subscribe((values: number[]) => {
+            expect(values).toEqual([ 3 ]);
+            done();
+        });
+    });
+});
+
+describe('MockNgRedux', () => {
+    beforeEach(() => {
+        MockNgRedux.reset();
+    });
+
+    test('should get a selector stub and emit a value', () => {
+        const selectorStub = MockNgRedux.getSelectorStub();
+        selectorStub.next(5);
+
+        selectorStub.subscribe(value => {
+            expect(value).toEqual(5);
+        });
+    });
+
+    test('should get a substore and emit a value', () => {
+        const mockSubStore = MockNgRedux.getSubStore(<any>'path', <any>'to', <any>'substore');
+
+        const selectorStub = mockSubStore.getSelectorStub('property');
+        selectorStub.next(10);
+
+        mockSubStore.select('property').subscribe(value => {
+            expect(value).toEqual(10);
+        });
+    });
+});
---
+++ projects/angular-redux2/store/mocks/services/ng-redux.service.mock.ts
@@ -0,0 +1,381 @@
+/**
+ * Import third-party library
+ */
+
+import { distinctUntilChanged, ReplaySubject } from 'rxjs';
+
+/**
+ * Import third-party types
+ */
+
+import type { Observable, Subject } from 'rxjs';
+import type { AnyAction, Reducer, Store, Unsubscribe } from 'redux';
+
+/**
+ * Angular-redux
+ */
+
+import { NgRedux } from '../../src/services/ng-redux.service';
+
+/**
+ * Angular-redux types
+ */
+
+import type { Comparator, PathSelector, Selector } from '../../src/interfaces/store.interface';
+
+/**
+ * Mocks type
+ */
+
+import type { SubStoreService } from '../../src/services/sub-store.service';
+import type { SelectorStubMap, SelectorStubRecord, SubStoreStubMap } from '../interfaces/store.interface.mock';
+
+/**
+ * This is the public of `@angular-redux2/store`.
+ * It wraps the global redux store and adds a few others add-on methods.
+ * It's what you'll inject into your Angular application as a service.
+ *
+ * `Convenience mock to make it easier to control selector behaviour in unit tests.`
+ */
+
+export class MockNgRedux<S = any> extends NgRedux<S> {
+    /**
+     * Mock substore
+     */
+
+    subStores: SubStoreStubMap = {};
+
+    /**
+     * Mock selectors
+     */
+
+    selections: SelectorStubMap = {};
+
+    /**
+     * Instance of NgRedux (singleton service).
+     * @hidden
+     */
+
+    protected static override instance: MockNgRedux<any>;
+
+    /**
+     * access to singleton service instance.
+     * @hidden
+     */
+
+    static override get store(): MockNgRedux<any> {
+        this.instance ||= new MockNgRedux<any>();
+
+        return this.instance;
+    }
+
+    /**
+     * Returns a subject that's connected to any observable returned by the
+     * given selector. You can use this subject to pump values into your
+     * components or services under test; when they call .select or @select
+     * in the context of a unit test, MockNgRedux will give them the values
+     * you pushed onto your stub.
+     *
+     * ```typescript
+     *   it('select ticket price data from the substore', async(() => {
+     *     const mockSubStore = MockNgRedux.getSubStore(
+     *       ['WALLABIES', 'items', 'id1']);
+     *
+     *     const selectorStub = mockSubStore.getSelectorStub('ticketPrice');
+     *     selectorStub.next(2);
+     *     selectorStub.complete();
+     *
+     *     animalComponent.ticketPrice$
+     *       .subscribe(
+     *         ticketPrice => expect(ticketPrice).toEqual(2));
+     *   }));
+     * ```
+     */
+
+    static getSelectorStub<R, S>(
+        selector?: Selector<R, S>,
+        comparator?: Comparator
+    ): Subject<S> {
+        return this.store.getSelectorStub<S>(
+            selector,
+            comparator
+        );
+    }
+
+    /**
+     * Returns a mock substore that allows you to set up selectorStubs for
+     * any 'fractal' stores your app creates with NgRedux.configureSubStore.
+     *
+     * If your app creates deeply nested sub stores from other sub store,
+     * pass the chain of pathSelectors in as ordered arguments to mock
+     * the nested sub store out.
+     *
+     * ```typescript
+     *
+     *   it('select name data from the substore', async(() => {
+     *     const mockSubStore = MockNgRedux.getSubStore(['WALLABIES', 'items', 'id1']);
+     *
+     *     const selectorStub = mockSubStore.getSelectorStub('name');
+     *     selectorStub.next('Wilbert');
+     *     selectorStub.complete();
+     *
+     *     animalComponent.name$
+     *       .subscribe(
+     *         name => expect(name).toEqual('Wilbert'));
+     *   }));
+     * ```
+     */
+
+    static getSubStore<S>(
+        ...pathSelectors: PathSelector[]
+    ): MockNgRedux<S> {
+        return pathSelectors.length
+            ? this.store.getSubStore(...pathSelectors)
+            : this.store;
+    }
+
+    /**
+     * Reset all previously configured stubs.
+     *
+     * ```typescript
+     * MockNgRedux.reset();
+     * ```
+     */
+
+    static reset(): void {
+        this.store.reset();
+    }
+
+    /**
+     * Returns a subject that's connected to any observable returned by the
+     * given selector. You can use this subject to pump values into your
+     * components or services under test; when they call .select or @select
+     * in the context of a unit test, MockNgRedux will give them the values
+     * you pushed onto your stub.
+     *
+     * ```typescript
+     *   it('select ticket price data from the substore', async(() => {
+     *     const mockSubStore = MockNgRedux.getSubStore(
+     *       ['WALLABIES', 'items', 'id1']);
+     *
+     *     const selectorStub = mockSubStore.getSelectorStub('ticketPrice');
+     *     selectorStub.next(2);
+     *     selectorStub.complete();
+     *
+     *     animalComponent.ticketPrice$
+     *       .subscribe(
+     *         ticketPrice => expect(ticketPrice).toEqual(2));
+     *   }));
+     * ```
+     */
+
+    getSelectorStub<SelectedState>(selector?: Selector<S, SelectedState>, comparator?: Comparator): Subject<SelectedState> {
+        return this.initSelectorStub<SelectedState>(selector, comparator).subject;
+    }
+
+    /**
+     * Returns a mock substore that allows you to set up selectorStubs for
+     * any 'fractal' stores your app creates with NgRedux.configureSubStore.
+     *
+     * If your app creates deeply nested sub stores from other sub store,
+     * pass the chain of pathSelectors in as ordered arguments to mock
+     * the nested sub store out.
+     *
+     * ```typescript
+     *
+     *   it('select name data from the substore', async(() => {
+     *     const mockSubStore = MockNgRedux.getSubStore(['WALLABIES', 'items', 'id1']);
+     *
+     *     const selectorStub = mockSubStore.getSelectorStub('name');
+     *     selectorStub.next('Wilbert');
+     *     selectorStub.complete();
+     *
+     *     animalComponent.name$
+     *       .subscribe(
+     *         name => expect(name).toEqual('Wilbert'));
+     *   }));
+     * ```
+     */
+
+    getSubStore<SubState>(...pathSelectors: PathSelector[]): MockNgRedux<SubState> {
+        const [ first, ...rest ] = pathSelectors;
+
+        return (first ? this.initSubStore(first).getSubStore(...rest) : this) as MockNgRedux<SubState>;
+    }
+
+    /**
+     * Reset all previously configured stubs.
+     *
+     * ```typescript
+     * MockNgRedux.reset();
+     * ```
+     */
+
+    reset(): void {
+        Object.keys(this.subStores).forEach(k => this.subStores[k].reset());
+        this.selections = {};
+        this.subStores = {};
+    }
+
+    /**
+     * `Configures a Mock Redux store`
+     *
+     * >Configures a Redux store and allows NgRedux to observe and dispatch to it.
+     *`This should only be called once for the lifetime of your app, for
+     * example in the constructor of your root component.`
+     */
+
+    override configureStore(): void {
+        return;
+    }
+
+    /**
+     * `Config mock sub-store`
+     * Carves off a 'subStore' or 'fractal' store from this one.
+     *
+     * >The returned object is itself an observable store, however any
+     * selections, dispatches, or invocations of localReducer will be
+     * specific to that sub-store and will not know about the parent
+     * ObservableStore from which it was created.
+     */
+
+    override configureSubStore<SubState>(basePath: PathSelector, _: Reducer<SubState>): SubStoreService<SubState> {
+        return this.initSubStore<SubState>(basePath);
+    }
+
+    /**
+     * `Mock provideStore`
+     *
+     * > Accepts a Redux store, then sets it in NgRedux and allows NgRedux to observe and dispatch to it.
+     * This should only be called once for the lifetime of your app, for example in the constructor of your root component.
+     * If configureStore has been used this cannot be used.
+     */
+
+    override provideStore(_: any, __: Store<any>): void {
+        return;
+    }
+
+    /**
+     * `Mock dispatch`
+     *
+     * > A `dispatching function` (or simply *dispatch function*) is a function that
+     * accepts an actions or an async actions; it then may or may not dispatch one
+     * or more actions to the store.
+     */
+
+    override dispatch<A extends AnyAction>(action: A): A {
+        return action;
+    }
+
+    /**
+     * `Mock replaceReducer`
+     *
+     * > Replaces the reducer currently used by the store to calculate the state.
+     * You might need this if your app implements code splitting, and you want to load some reducers dynamically.
+     * You might also need this if you implement a hot reloading mechanism for Redux.
+     */
+
+    override replaceReducer(): null {
+        return null;
+    }
+
+    /**
+     * `Mock getState`
+     * > Get store state
+     */
+
+    override getState(): S {
+        return <S>{};
+    }
+
+    /**
+     * `Mock subscribe`
+     *
+     * > Adds a change listener.
+     * It will be called any time an actions is dispatched, and some part of the state tree may potentially have changed.
+     * You may then call getState() to read the current state tree inside the callback.
+     */
+
+    override subscribe(): Unsubscribe {
+        return () => null;
+    }
+
+    /**
+     * `Mock Select`
+     * > Select a slice of state to expose as an observable.
+     */
+
+    override select<SelectedState>(selector?: Selector<any, SelectedState>, comparator?: Comparator): Observable<any> {
+        const stub = this.initSelectorStub<SelectedState>(selector, comparator);
+
+        return stub.comparator
+            ? stub.subject.pipe(distinctUntilChanged(stub.comparator))
+            : stub.subject;
+    }
+
+
+    /**
+     * Init new mock sub-store
+     * @hidden
+     */
+
+    protected initSubStore<SubState>(basePath: PathSelector): any {
+        const result = this.subStores[JSON.stringify(basePath)] || new MockNgRedux<SubState>();
+        this.subStores[JSON.stringify(basePath)] = result;
+
+        return result;
+    }
+
+    /**
+     * Init selector stub
+     * @hidden
+     */
+
+    protected initSelectorStub<SelectedState>(
+        selector?: Selector<S, SelectedState>,
+        comparator?: Comparator
+    ): SelectorStubRecord {
+        let signature = selector ? selector.toString() : '';
+
+        if ('function' === typeof selector) {
+            signature = this.coverageFunctionSignature(signature);
+            /**
+             * Run original coverage function
+             */
+
+            if ((window as any)['__coverage__']) {
+                try {
+                    selector(<any>{});
+                } catch (e) { /* empty */
+                }
+            }
+        }
+
+        const record = this.selections[signature] || {
+            subject: new ReplaySubject<SelectedState>(),
+            comparator,
+        };
+
+        this.selections[signature] = record;
+
+        return record;
+    }
+
+    /**
+     * Remove coverage metadata from function signature.
+     * and create uniq signature for stub selection.
+     *
+     * @param signature - function signature
+     * @protected
+     */
+
+    protected coverageFunctionSignature(
+        signature: string,
+    ): string {
+        signature = signature.replace(/cov_.+?[,;]/g, '');
+        signature = signature.replace(/return/g, '');
+        signature = signature.replace(/[^A-Za-z0-9]/g, '');
+
+        return signature;
+    }
+}
