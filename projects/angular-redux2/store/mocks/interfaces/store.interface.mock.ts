bba06cb61824d0d89a75537c99c819d8c0887c3d
Garefild <Garefild2@gmail.com>
2023-04-29 02:00:49 +0300

    store.interface.mock: add a mock types

---
+++ projects/angular-redux2/store/mocks/interfaces/store.interface.mock.ts
@@ -0,0 +1,53 @@
+/**
+ * Imports third-party types
+ */
+
+import type { Subject } from 'rxjs';
+
+/**
+ * angular-redux2 types
+ */
+
+import type { Comparator } from '@angular-redux2/store';
+import type { MockNgRedux } from '../services/ng-redux.service.mock';
+
+/**
+ * Represents a selector stub record.
+ * @interface
+ * @property {Subject} subject - The subject of the stub record.
+ * @property {Comparator} comparator - The comparator function to use for the stub record.
+ */
+
+export interface SelectorStubRecord {
+    subject: Subject<any>;
+    comparator: Comparator;
+}
+
+/**
+ * A mapping of selectors to their respective stub records.
+ * @interface
+ */
+
+export interface SelectorStubMap {
+    /**
+     * The selector key of the stub record.
+     * @type {string}
+     * @name SelectorStubMap#[selector
+     */
+
+    [selector: string]: SelectorStubRecord;
+}
+
+/**
+ * A map of sub store stubs where the keys are strings representing the base path and values are MockNgRedux objects.
+ *
+ * @interface
+ * @name SubStoreStubMap
+ *
+ * @property {string} basePath - The base path of the sub store stub.
+ * @property {MockNgRedux} any - The sub store stub instance.
+ */
+
+export interface SubStoreStubMap {
+    [basePath: string]: MockNgRedux<any>;
+}
