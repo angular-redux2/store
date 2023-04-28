0af4fff917f2b0426ebb92f4228d2883ac3f1ee1
Garefild <Garefild2@gmail.com>
2023-04-29 02:01:39 +0300

    public-api: add export for angular-redux2 and mocks

---
+++ projects/angular-redux2/store/src/public-api.ts
@@ -0,0 +1,24 @@
+/*
+ * Public API Surface of store
+ */
+
+export * from './ng-redux.module';
+
+// Abstract
+export * from './abstract/reducer.abstract';
+
+// Services
+export * from './services/ng-redux.service';
+export * from './services/dev-tool.service';
+export * from './services/sub-store.service';
+export * from './services/reducer.service';
+
+// Interfaces
+export * from './interfaces/store.interface';
+export * from './interfaces/reducer.interface';
+
+// Decorators
+export * from './decorators/action.decorator';
+export * from './decorators/select.decorator';
+export * from './decorators/dispatch.decorator';
+export * from './decorators/substore.decorator';
