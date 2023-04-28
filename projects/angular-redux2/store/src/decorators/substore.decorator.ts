9ef44aef92b38a4149dab787cd8afd9a821b8a80
Garefild <Garefild2@gmail.com>
2023-04-29 01:55:30 +0300

    substore.decorator: refactor

--- projects/angular-redux2/store/src/decorators/substore.decorator.ts
+++ projects/angular-redux2/store/src/decorators/substore.decorator.ts
@@ -1,30 +1,34 @@
 /**
- * Imports third-party libraries
+ * Import third-party types
  */
 
-import { AnyAction, Reducer } from 'redux';
+import type { AnyAction, Reducer } from 'redux';
 
 /**
- * Import interfaces
+ * angular-redux2
  */
 
 import { LOCAL_REDUCER_KEY } from '../interfaces/fractal.interface';
 
 /**
- * Modifies the behaviour of any `@Select`, `@Select$`, or `@Dispatch`
- * decorators to operate on a substore defined by the FractalStoreOptions.
+ * A decorator function that modifies the behavior of any `@Select`, `@Select$`, or `@Dispatch`
+ * decorators to operate on a substore defined with a given reducer.
  *
+ * @example
  * ```typescript
  * @SubStore(userComponentReducer)
  * export class TestComponent implements OnInit {
  *     @Input() userId: String;
  *
  *     // The substore will be created from the return value of this function.
- *     getBasePath(): Array<string | String> | null {
+ *     basePath(): Array<string | String> | null {
  *         return this.userId ? ['users', this.userId] : null;
  *     }
  * }
  * ```
+ *
+ * @param reducer - The reducer function used to create the substore.
+ * @returns A ClassDecorator function that sets the reducer to the class constructor's LOCAL_REDUCER_KEY property.
  */
 
 export function Substore(reducer: Reducer<any, AnyAction>): ClassDecorator {
