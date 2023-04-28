298d47ea7f10b44dbe581f1935f9c7f9aec60d8b
Garefild <Garefild2@gmail.com>
2023-04-29 01:53:37 +0300

    object.component: refactor

--- projects/angular-redux2/store/src/components/object.component.spec.ts
+++ projects/angular-redux2/store/src/components/object.component.spec.ts
@@ -1,176 +1,224 @@
 /**
- * Import components
+ * angular-redux2
  */
 
-import { get, set } from './object.component';
+import { get, set, shallowCopy } from './object.component';
 
-/**
- * Gets a deeply-nested property value from an object
- */
-
-describe('Gets a deeply-nested property value from an object', () => {
-    test('should select a first-level prop', () => {
-        const test = { foo: 1 };
-        expect(get(test, [ 'foo' ])).toBe(1);
+const storeStruct = {
+    foo: 1,
+    a: false,
+    b: 0,
+    c: '',
+    d: undefined,
+    level_1: {
+        a: false,
+        b: 0,
+        c: '',
+        d: undefined,
+        bar: 2,
+        level_2: {
+            fooBar: 3,
+            level_3: {
+                key: 'x',
+            }
+        },
+        arr: [
+            'test',
+            'array',
+            'string'
+        ]
+    }
+};
+describe('shallowCopy', () => {
+    test('should create a shallow copy of an object', () => {
+        const obj = { a: 1, b: { c: 2 } };
+        const copy = shallowCopy(obj);
+        expect(copy).toEqual(obj);
+        expect(copy).not.toBe(obj);
     });
 
-    test('should select a second-level prop', () => {
-        const test = { foo: { bar: 2 } };
-        expect(get(test, [ 'foo', 'bar' ])).toBe(2);
+    test('should create a shallow copy of an array', () => {
+        const arr = [ 1, 2, { a: 3 }];
+        const copy = shallowCopy(arr);
+        expect(copy).toEqual(arr);
+        expect(copy).not.toBe(arr);
     });
 
-    test('should select a third-level prop', () => {
-        const test = { foo: { bar: { quux: 3 } } };
-        expect(get(test, [ 'foo', 'bar', 'quux' ])).toBe(3);
-    });
-
-    test('should select falsy values properly', () => {
-        const test = {
-            a: false,
-            b: 0,
-            c: '',
-            d: undefined,
-        };
-        expect(get(test, [ 'a' ])).toBe(false);
-        expect(get(test, [ 'b' ])).toBe(0);
-        expect(get(test, [ 'c' ])).toBe('');
-        expect(get(test, [ 'd' ])).toBe(undefined);
-    });
-
-    test('should select nested falsy values properly', () => {
-        const test = {
-            foo: {
-                a: false,
-                b: 0,
-                c: '',
-                d: undefined,
-            },
-        };
-        expect(get(test, [ 'foo', 'a' ])).toBe(false);
-        expect(get(test, [ 'foo', 'b' ])).toBe(0);
-        expect(get(test, [ 'foo', 'c' ])).toBe('');
-        expect(get(test, [ 'foo', 'd' ])).toBe(undefined);
-    });
-
-    test('should not freak if the object is null', () =>
-        expect(get(null, [ 'foo', 'd' ])).toBe(null));
-
-    test('should not freak if the object is undefined', () =>
-        expect(get(undefined, [ 'foo', 'd' ])).toBe(undefined));
-
-    test('should not freak if the object is a primitive', () =>
-        expect(get(42, [ 'foo', 'd' ])).toBe(undefined));
-
-    test('should return undefined for a nonexistent prop', () => {
-        const test = { foo: 1 };
-        expect(get(test, [ 'bar' ])).toBe(undefined);
-    });
-
-    test('should return undefined for a nonexistent path', () => {
-        const test = { foo: 1 };
-        expect(get(test, [ 'bar', 'quux' ])).toBe(undefined);
-    });
-
-    test('should return undefined for a nested nonexistent prop', () => {
-        const test = { foo: 1 };
-        expect(get(test, [ 'foo', 'bar' ])).toBe(undefined);
-    });
-
-    test('should select array elements properly', () => {
-        const test = [ 'foo', 'bar' ];
-        expect(get(test, [ 0 ])).toBe('foo');
-        expect(get(test, [ '0' ])).toBe('foo');
-        expect(get(test, [ 1 ])).toBe('bar');
-        expect(get(test, [ '1' ])).toBe('bar');
-        expect(get(test, [ 2 ])).toBe(undefined);
-        expect(get(test, [ '2' ])).toBe(undefined);
-    });
-
-    test('should select nested array elements properly', () => {
-        const test = { quux: [ 'foo', 'bar' ] };
-        expect(get(test, [ 'quux', 0 ])).toBe('foo');
-        expect(get(test, [ 'quux', '0' ])).toBe('foo');
-        expect(get(test, [ 'quux', 1 ])).toBe('bar');
-        expect(get(test, [ 'quux', '1' ])).toBe('bar');
-        expect(get(test, [ 'quux', 2 ])).toBe(undefined);
-        expect(get(test, [ 'quux', '2' ])).toBe(undefined);
-    });
-
-    test('should defer to a native get function if it exists on the data', () => {
-        const testPath = [ 'foo', 'bar' ];
-        const test = {
-            getIn: (path: (string | number)[]) => path === testPath ? 42 : undefined,
-        };
-
-        expect(get(test, testPath)).toBe(42);
-        expect(get(test, [ 'some', 'path' ])).toBe(undefined);
+    test('should return empty object for undefined input', () => {
+        const obj = undefined;
+        const copy = shallowCopy(obj);
+        expect(copy).toEqual({});
     });
 });
 
-/**
- * Sets a deeply-nested property value from an object,
- * given a 'path' of property names or array indices.
- */
-
-describe('Sets a deeply-nested property value from an object, given a \'path\' of property names or array indices.', () => {
-    test('performs a shallow set correctly without mutation', () => {
-        const original = { a: 1 };
-
-        expect(set(original, [ 'b' ], 2)).toStrictEqual({ a: 1, b: 2 });
-        expect(original).toStrictEqual({ a: 1 });
+describe('get', () => {
+    test('should return the value at the specified path', () => {
+        const obj = { a: { b: { c: 1 } } };
+        expect(get(obj, [ 'a', 'b', 'c' ])).toBe(1);
     });
 
-    test('performs a deeply nested set correctly without mutation', () => {
-        const original = { a: 1 };
-        const expected = {
-            a: 1,
-            b: {
-                c: {
-                    d: 2,
+    test('should return undefined for invalid path', () => {
+        const obj = { a: { b: { c: 1 } } };
+        expect(get(obj, [ 'a', 'b', 'd' ])).toBeUndefined();
+        expect(get(obj, [ 'a', 'c', 'd' ])).toBeUndefined();
+        expect(get(obj, [ 'd' ])).toBeUndefined();
+    });
+
+    test('should return the root object for empty path', () => {
+        const obj = { a: { b: { c: 1 } } };
+        expect(get(obj, [])).toEqual(obj);
+    });
+
+    describe('should get a deeply nested property value from an object', () => {
+        describe.each([
+            [[ 'foo' ], 1 ],
+            [[ 'level_1', 'bar' ], 2 ],
+            [[ 'level_1', 'level_2', 'fooBar' ], 3 ],
+        ])('get() should select a deeply nested property', (path, expected) => {
+            test(`path ${JSON.stringify(path)} should return ${expected}`, () => {
+                expect(get(storeStruct, path)).toEqual(expected);
+            });
+        });
+
+        describe.each([
+            [ 'a', false ],
+            [ 'b', 0 ],
+            [ 'c', '' ],
+            [ 'd', undefined ],
+        ])('get() should select falsy values properly', (prop, expected) => {
+            test(`property '${prop}' should return ${expected}`, () => {
+                expect(get(storeStruct, [ prop ])).toEqual(expected);
+            });
+        });
+
+        describe.each([
+            [ 'a', false ],
+            [ 'b', 0 ],
+            [ 'c', '' ],
+            [ 'd', undefined ],
+        ])('get() should select nested falsy values properly', (prop, expected) => {
+            test(`property 'level_1.${prop}' should return ${expected}`, () => {
+                expect(get(storeStruct, [ 'level_1', prop ])).toEqual(expected);
+            });
+        });
+
+        test('get() should not panic if the object is undefined', () => {
+            expect(get(undefined, [ 'foo', 'd' ])).toEqual(undefined);
+        });
+
+        test('get() should not panic if the object is a primitive', () => {
+            expect(get(42, [ 'foo', 'd' ])).toEqual(undefined);
+        });
+
+        test('get() should return undefined for a nonexistent prop', () => {
+            expect(get(storeStruct, [ 'bar' ])).toEqual(undefined);
+        });
+
+        test('get() should return undefined for a nonexistent path', () => {
+            expect(get(storeStruct, [ 'bar', 'test' ])).toEqual(undefined);
+        });
+
+        describe.each([
+            [[ 0 ], 'foo' ],
+            [[ '0' ], 'foo' ],
+            [[ 1 ], 'bar' ],
+            [[ '1' ], 'bar' ],
+            [[ 2 ], undefined ],
+            [[ '2' ], undefined ],
+        ])('get() should select array elements properly', (path, expected) => {
+            test(`path ${JSON.stringify(path)} should return ${expected}`, () => {
+                const test = [ 'foo', 'bar' ];
+                expect(get(test, path)).toEqual(expected);
+            });
+        });
+    });
+
+    describe.each([
+        [[ 'test', 0 ], 'foo' ],
+        [[ 'test', '0' ], 'foo' ],
+        [[ 'test', 1 ], 'bar' ],
+        [[ 'test', '1' ], 'bar' ],
+        [[ 'test', 2 ], undefined ],
+        [[ 'test', '2' ], undefined ],
+    ])('get() should select nested array elements properly', (path, expected) => {
+        test(`path ${JSON.stringify(path)} should return ${JSON.stringify(expected)}`, () => {
+            const test = { test: [ 'foo', 'bar' ] };
+            expect(get(test, path)).toBe(expected);
+        });
+    });
+
+    test('Should return an array and allow find function.', () => {
+        const array = get(storeStruct, [ 'level_1', 'arr' ]);
+        const result = array.find((value: string) => value == 'test');
+        expect(result).toStrictEqual('test');
+    });
+});
+
+describe('set', () => {
+    test('should set the value at the specified path', () => {
+        const obj = { a: { b: { c: 1 } } };
+        const copy = set(obj, [ 'a', 'b', 'c' ], 2);
+        expect(copy).toEqual({ a: { b: { c: 2 } } });
+        expect(copy).not.toBe(obj);
+    });
+
+    test('should create intermediate keys if they do not exist', () => {
+        const obj = { a: { b: {} } };
+        const copy = set(obj, [ 'a', 'b', 'c' ], 1);
+        expect(copy).toEqual({ a: { b: { c: 1 } } });
+    });
+
+    test('should return input object for undefined path', () => {
+        const obj = { a: 1 };
+        const copy = set(obj, <any>undefined, 2);
+        expect(copy).toBe(obj);
+    });
+
+    test('should return input object for null path', () => {
+        const obj = { a: 1 };
+        const copy = set(obj, <any>null, 2);
+        expect(copy).toBe(obj);
+    });
+
+    test('should return input object for empty path', () => {
+        const obj = { a: 1 };
+        const copy = set(obj, [], 2);
+        expect(copy).toEqual(obj);
+    });
+
+    describe('Should sets a deeply-nested property value and return a new object.', () => {
+        describe.each([
+            [ undefined, [ 'foo', 'bar' ], 5, undefined ],
+            [{ arr: [ 1, 2, 3, 4 ] }, [ 'arr', '0' ], 2, { arr: [ 2, 2, 3, 4 ] }],
+            [{ a: 1 }, [ 'b' ], 2, { a: 1, b: 2 }],
+            [{ a: 1 }, [ 'b', 'c', 'd' ], 2, {
+                a: 1,
+                b: {
+                    c: {
+                        d: 2,
+                    },
                 },
-            },
-        };
-
-        expect(set(original, [ 'b', 'c', 'd' ], 2)).toStrictEqual(expected);
-        expect(original).toStrictEqual({ a: 1 });
-    });
-
-    test('performs a deeply nested set with existing keys without mutation', () => {
-        const original = {
-            a: 1,
-            b: {
-                wat: 3,
-            },
-        };
-
-        const expected = {
-            a: 1,
-            b: {
-                wat: 3,
-                c: {
-                    d: 2,
+            }],
+            [{ a: 1, b: { wat: 3 } }, [ 'b', 'c', 'd' ], 2, {
+                a: 1,
+                b: {
+                    wat: 3,
+                    c: {
+                        d: 2,
+                    },
                 },
-            },
-        };
+            }],
+        ])('set', (original, path, value, expected) => {
+            test(`returns ${JSON.stringify(expected)}`, () => {
+                expect(set(original, path, value)).toEqual(expected);
+            });
 
-        expect(set(original, [ 'b', 'c', 'd' ], 2)).toStrictEqual(expected);
-        expect(original).toStrictEqual({ a: 1, b: { wat: 3 } });
-    });
-
-    test('should use set method of an object (case of ImmutableJS)', () => {
-        let setCalled = false;
-
-        class TestClass {
-            setIn() {
-                setCalled = true;
-            }
-        }
-
-        const original = {
-            root: new TestClass(),
-        };
-
-        set(original, [ 'root', 'a', 'b', 'c' ], 123);
-        expect(setCalled).toBe(true);
+            test('does not mutate the original object', () => {
+                if (original) {
+                    expect(set(original, path, value)).not.toBe(original);
+                    expect(original).toEqual(original);
+                }
+            });
+        });
     });
 });
--- projects/angular-redux2/store/src/components/object.component.ts
+++ projects/angular-redux2/store/src/components/object.component.ts
@@ -1,72 +1,89 @@
 /**
- * Gets a deeply-nested property value from an object, given a 'path'
- * of property names or array indices.
+ * Shallowly copies an array or object.
+ * @param {any} object - The array or object to copy.
+ * @returns {any} A shallow copy of the input array or object.
+ */
+
+export function shallowCopy(object: any): any {
+    if (Array.isArray(object)) {
+        return object.slice();
+    }
+
+    return Object.assign({}, object);
+}
+
+/**
+ * Returns the value at the specified path in the given object.
  *
+ * @example
  * ```typescript
  * const test = { foo: { bar: 2 } };
  * get(test, [ 'foo', 'bar' ]) // to be 2
  * ```
  *
- * @param object - the object to get a value from.
- * @param path - path to value.
+ * @param {object} object - The object to get the value from.
+ * @param {Array<string|number>} path - The path to the value.
+ * @returns {*} - The value at the specified path, or undefined if the path is not valid.
  */
 
 export function get(object: any, path: Array<string | number>): any {
-    if (!object) {
-        return object;
+    let result = object;
+
+    for (const key of path) {
+        if (typeof result === 'object' && result !== null) {
+            result = Reflect.get(result, key);
+        } else {
+            return undefined;
+        }
     }
 
-    // If this is an ImmutableJS structure, use existing getIn function
-    if ('function' === typeof object.getIn) {
-        return object.getIn(path);
-    }
-
-    const [ firstElem, ...restElems ] = path;
-
-    if (undefined === object[firstElem]) {
-        return undefined;
-    }
-
-    if (restElems.length === 0) {
-        return object[firstElem];
-    }
-
-    return get(object[firstElem], restElems);
+    return result;
 }
 
 /**
- * Sets a deeply-nested property value from an object, given a 'path'
- * of property names or array indices. Path elements are created if
- * not there already. Does not mutate the given object.
+ * Sets the value at the given path in the object.
+ * If any intermediate keys in the path do not exist, they are created as empty objects.
+ * A shallow copy of the object is made to avoid mutating the original object.
  *
+ * @example
  * ```typescript
  * const original = {
- *  a: 1,
- *  b: {
- *   wat: 3,
- *  },
+ *      a: 1,
+ *      b: {
+ *          wat: 3,
+ *      },
  * };
  *
  * set(original, [ 'b', 'c', 'd' ], 2)
  * ```
  *
- * @param object - the object to set a value to.
- * @param path - path to location.
- * @param value - the value to set in path location.
+ * @param object - The object to set the value in.
+ * @param path - The path to the key whose value should be set.
+ * @param value - The value to set at the key.
+ * @returns A shallow copy of the object with the new value set at the given path.
  */
 
-export function set(object: any, [ firstElem, ...restElems ]: Array<string | number>, value: any): Object {
-
-    // If this is an ImmutableJS structure, use existing setIn function
-    if ('function' === typeof (object[firstElem] || {}).setIn) {
-        return {
-            ...object,
-            [firstElem]: object[firstElem].setIn(restElems, value),
-        };
+export function set(object: any, path: Array<string | number>, value: any): any {
+    if (!object || !Array.isArray(path) || path.length < 1) {
+        return object;
     }
 
-    return {
-        ...object,
-        [firstElem]: restElems.length === 0 ? value : set(object[firstElem] || {}, restElems, value),
-    };
+    const result = shallowCopy(object);
+    let tempObjet = result;
+
+    for (let i = 0; i < path.length - 1; i++) {
+        const key = path[i];
+
+        if (!tempObjet[key] || typeof tempObjet[key] !== 'object') {
+            tempObjet[key] = {};
+        } else {
+            tempObjet[key] = shallowCopy(tempObjet[key]);
+        }
+
+        tempObjet = tempObjet[key];
+    }
+
+    tempObjet[path[path.length - 1]] = value;
+
+    return result;
 }
