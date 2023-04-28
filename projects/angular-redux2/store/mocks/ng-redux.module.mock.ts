ee97dd165da333c395820751806e84342f464288
Garefild <Garefild2@gmail.com>
2023-04-29 01:35:13 +0300

    ng-redux.module.mock: add ng-redux module mock

---
+++ projects/angular-redux2/store/mocks/ng-redux.module.mock.ts
@@ -0,0 +1,41 @@
+/**
+ * Import third-party libraries
+ */
+
+import { NgModule } from '@angular/core';
+
+/**
+ * angular-redux2
+ */
+
+import { NgRedux, DevToolsExtension } from '@angular-redux2/store';
+
+/**
+ * Mocks
+ */
+
+import { MockNgRedux } from './services/ng-redux.service.mock';
+import { MockDevToolsExtension } from './services/dev-tool.service.mock';
+
+/**
+ * Mock factory
+ * @hidden
+ */
+
+export function _mockNgReduxFactory() {
+    return MockNgRedux.store;
+}
+
+/**
+ * NgRedux module
+ */
+
+@NgModule({
+    imports: [],
+    providers: [
+        { provide: NgRedux, useFactory: _mockNgReduxFactory  },
+        { provide: DevToolsExtension, useClass: MockDevToolsExtension }
+    ],
+})
+export class MockNgReduxModule {
+}
