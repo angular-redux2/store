2d684238a270279e74e89e47be961f9be89abdbf
Patryk Kozłowski <pakozlowski@deloittece.com>
2023-10-20 13:30:37 +0200

    fix failing UT

--- projects/angular-redux2/store/src/services/reducer.service.ts
+++ projects/angular-redux2/store/src/services/reducer.service.ts
@@ -2,7 +2,7 @@
  * Import third-party types
  */
 
-import {AnyAction, Reducer} from 'redux';
+import type {AnyAction, Reducer} from 'redux';
 
 /**
  * angular-redux2
@@ -15,7 +15,7 @@ import {get, set, shallowCopy} from '../components/object.component';
  * angular-redux2 types
  */
 
-import {Middleware, NextMiddleware} from '../interfaces/reducer.interface';
+import type {Middleware, NextMiddleware} from '../interfaces/reducer.interface';
 
 /**
  * Service class for composing reducers and applying middleware to them.
@@ -180,6 +180,13 @@ export class ReducerService {
         }
     }
 
+    private static shouldCreateProxyForProperty(property: any): boolean {
+        return property != null
+            && !(property instanceof Date)
+            && typeof property === 'object'
+            && !property._isProxy;
+    }
+
     /**
      * The root reducer function for sub-stores.
      *
@@ -292,10 +299,7 @@ export class ReducerService {
                 if (prop === '_target') return target;
                 if (prop === '_isProxy') return true;
 
-                if (target[prop] !== null &&
-                    !(target instanceof Date || (target[prop] instanceof Date)) &&
-                    typeof target[prop] === 'object' && !target[prop]._isProxy) {
-
+                if (ReducerService.shouldCreateProxyForProperty(target[prop])) {
                     const currentStack = receiver._stack || [];
                     (this as any)._stack = [...currentStack, prop];
 
